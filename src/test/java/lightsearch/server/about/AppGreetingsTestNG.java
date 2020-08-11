b3acee4489c2395b6672554b23b00d0d873546c3
viise <viise@outlook.com>
2020-08-11 16:21:00 +1000

    Added tests for "about" package

---
+++ src/test/java/lightsearch/server/about/AppGreetingsTestNG.java
@@ -0,0 +1,32 @@
+/*
+ *    Copyright 2020 ViiSE
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package lightsearch.server.about;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class AppGreetingsTestNG {
+
+    @Test
+    public void greetings() {
+        AppGreetings app = new AppGreetingsImpl(() -> "Test");
+        System.out.println(app.greetings());
+
+        assertTrue(app.greetings().contains("Test"));
+    }
+}
