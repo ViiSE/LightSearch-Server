1d24dd103c3ba5e0b200371035b648a2fed06125
viise <viise@outlook.com>
2020-10-02 17:15:21 +1000

    Fixed checker

--- src/main/java/lightsearch/server/checker/CommandCheckerAdminDelBlacklistListImpl.java
+++ src/main/java/lightsearch/server/checker/CommandCheckerAdminDelBlacklistListImpl.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2019 ViiSE.
+ *    Copyright 2020 ViiSE
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package lightsearch.server.checker;
@@ -42,6 +42,8 @@ public class CommandCheckerAdminDelBlacklistListImpl implements Checker<AdminCom
             throw new CheckerException("Wrong command format. List of IMEI is null!", "AddBlacklist: unknown clients: List of IMEI is null.");
 
         for(String IMEIStr: command.IMEI()) {
+            if(checker.isNull(IMEIStr))
+                throw new CheckerException("Wrong command format. One or more IMEI in list of IMEI is null!", "AddBlacklist: unknown client: one or more IMEI in list of IMEI is null.");
             if (checker.isEmpty(IMEIStr))
                 throw new CheckerException("Wrong command format. One or more IMEI in list of IMEI is empty!", "AddBlacklist: unknown client: one or more IMEI in list of IMEI is empty.");
             if(!blacklistService.contains(IMEIStr))
--- src/main/java/lightsearch/server/checker/CommandCheckerAdminKickListClientImpl.java
+++ src/main/java/lightsearch/server/checker/CommandCheckerAdminKickListClientImpl.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2019 ViiSE.
+ *    Copyright 2020 ViiSE
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package lightsearch.server.checker;
@@ -48,9 +48,10 @@ public class CommandCheckerAdminKickListClientImpl implements Checker<AdminComma
             throw new CheckerException("Wrong command format. IMEI is null!", "AddBlacklist: unknown client: IMEI is null.");
 
         for(String IMEI: command.IMEI()) {
+            if(checker.isNull(IMEI))
+                throw new CheckerException("Wrong command format. One or more IMEI is null!", "AddBlacklist: unknown client: one or more IMEI is null.");
             if (checker.isEmpty(IMEI))
                 throw new CheckerException("Wrong command format. One or more IMEI is empty!", "AddBlacklist: unknown client: one or more IMEI is empty.");
-
             if (!clientsService.contains(IMEI))
                 throw new CheckerException("One or more clients with current IMEI not found. (Not connected to LightSearch Server)", "One or more clients does not exist.");
         }
