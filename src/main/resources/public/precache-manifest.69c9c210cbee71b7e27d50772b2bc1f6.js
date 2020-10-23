243f01319acf775dca175b54af6b859dcff41c31
viise <viise@outlook.com>
2020-10-23 16:57:39 +1000

    Updated Admin Panel

---
+++ src/main/resources/public/precache-manifest.69c9c210cbee71b7e27d50772b2bc1f6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220665c3f0edd63377adba8f1962fef6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d35f4667c0d49e1703",
+    "url": "/static/css/2.02c0b7dd.chunk.css"
+  },
+  {
+    "revision": "ebff6acdc5fda4ba6431",
+    "url": "/static/css/main.2d75d37b.chunk.css"
+  },
+  {
+    "revision": "72d35f4667c0d49e1703",
+    "url": "/static/js/2.22fd95f8.chunk.js"
+  },
+  {
+    "revision": "5fbddd48df7fb1dd84a303a4a773a662",
+    "url": "/static/js/2.22fd95f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebff6acdc5fda4ba6431",
+    "url": "/static/js/main.1ebf4f87.chunk.js"
+  },
+  {
+    "revision": "672153cbfb88f560bb0e",
+    "url": "/static/js/runtime-main.5607e164.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "e65af6f73b8b7c718eed1088d4c44660",
+    "url": "/static/media/ls_logo_big.e65af6f7.png"
+  },
+  {
+    "revision": "54d4b73c7b2a655854efc60aaaf17aeb",
+    "url": "/static/media/ls_logo_svg.54d4b73c.svg"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/static/media/primeicons.79432094.woff"
+  }
+]);
